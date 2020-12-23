4c5e0da4c4a6b3ea1b8f2e2d3a3ea35e69aa172f
Shawn Tung 董劭杰 <67937453+pourquoiaimer@users.noreply.github.com>
2020-12-23 09:53:57 +0800

    把vue放上去

---
+++ vue備忘-串接firebase/all.js
@@ -0,0 +1,186 @@
+const db = firebase.database();
+var app = new Vue({
+    el: '#app',
+    data: {
+        newTodo: '',
+        todos: [//creat裡面去更新資料庫
+        ],
+        cacheTodo: {},
+        cacheTitle: {},
+        visibility: 'all',
+    },
+    methods: {
+        addTodo: function () {
+            let value = this.newTodo.trim();
+            let timesId = Math.floor(Date.now());
+            if (!value) {
+                return;
+            }
+            this.todos.push({
+                id: timesId,
+                title: value,
+                completed: false,
+            });
+            this.newTodo = "";
+            setDataOn(this);
+            //抓陣列進去資料庫
+        },
+        removeTodo: function (thisTodo) { //刪去todo事項        
+            let newIndex = this.todos.findIndex(function (item, key) { //在todos陣列中，找尋id與現在點擊的id一樣的，傳出其陣列序號
+                return thisTodo.id === item.id;
+            })
+            this.todos.splice(newIndex, 1);  //在todos陣列中刪去該序號的值
+            setDataOn(this);
+        },
+        editTodo: function (item) {
+            this.cacheTodo = item;
+            this.cacheTitle = item.title;
+        },
+        cancelEdit: function () {
+            this.cacheTodo = "";
+
+        },
+        doneEdit: function (item) {
+            item.title = this.cacheTitle;
+            this.cacheTitle = "";
+            this.cacheTodo = {};
+            //更新資料庫
+            setDataOn(this);
+        },
+        removeAll: function () {
+            this.todos = [];
+            setDataOn(this);
+        },
+        removeCompleted: function () {
+            this.todos = this.todos.filter(function (item) {
+                return !item.completed;
+            })
+            setDataOn(this);
+        },
+    },
+    computed: {
+        filterTodos: function () {
+            let newTodos = [];
+            switch (true) {
+                case (this.visibility == "all"):
+                    return this.todos;
+                    break;
+                case (this.visibility == "active"):
+                    this.todos.forEach(function (item) {
+                        if (!item.completed) {
+                            newTodos.push(item);
+                        }
+                    })
+                    return newTodos;
+                    break;
+                case (this.visibility == "completed"):
+                    this.todos.forEach(function (item) {
+                        if (item.completed) {
+                            newTodos.push(item);
+                        }
+                    })
+                    return newTodos;
+                    break;
+                default:
+            }
+        },
+        countTodos: function () {
+            let newTodos = []
+            this.todos.forEach(function (item) {
+                if (!item.completed) {
+                    newTodos.push(item);
+                }
+            })
+            return newTodos.length;
+        },
+    },
+    created() {
+        getDataFirst(this);
+    },
+})
+
+
+
+//firebase的
+
+function getDataFirst(datas) {
+    db.ref('/todoList/mytodo').once('value', function (snapshot) {
+        let data = snapshot.val();
+        console.log(typeof (data));
+        console.log(data[0]);
+        if (!data) {
+            return;
+        } else {
+            for (let i = 0; i < data.length; i++) {
+                datas.todos.push(data[i]);
+            }
+        }
+    });
+}
+
+
+
+
+function setDataOn(data) {
+    db.ref('/todoList/mytodo').set(data.todos).then(function () {
+        console.log("建立成功");
+    }).catch(function () {
+        alert('可能是伺服器錯誤，請稍後再試');
+    });
+}
+
+
+function getdataAll() {  //測試上傳，算是成功吧，暫不使用或至少不動到testAll這個資料夾
+    for (let i = 0; i < dataName.length; i++) {
+        console.log(dataName.length);
+        $.ajax({
+            type: "Get",
+            url: `./test/${dataName[i].name}`,
+            dataType: 'json',
+            contentType: "application/xml; charset=UTF-8",
+            success: function (data) {
+                db.ref(`/testData/${dataName[i].name}`).set(data
+                ).then(function () {
+                    console.log("建立成功");
+                }).catch(function () {
+                    console.log('可能是伺服器錯誤，請稍後再試');
+                });
+            }
+        });
+    }
+}
+
+function pushTest(data) {
+    console.log(data);
+    db.ref('/').push({
+        data
+    }).then(function () {
+        alert("建立成功");
+    }).catch(function () {
+        alert('可能是伺服器錯誤，請稍後再試');
+    });
+}
+
+function setToFirebase() {
+    console.log(dataAll);
+    db.ref('/').set([dataAll[0], dataAll[1]]
+    ).then(function () {
+        alert("建立成功");
+    }).catch(function () {
+        alert('可能是伺服器錯誤，請稍後再試');
+    });
+}
+
+function updateToFirebase() {
+    db.ref('/friend').update({
+        item
+    }).then(function () {
+        alert("建立成功");
+    }).catch(function () {
+        alert('可能是伺服器錯誤，請稍後再試');
+    });
+}
+
+function cleanAll() {
+    db.ref().remove();
+}
