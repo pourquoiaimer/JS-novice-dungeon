a1e3aa68619ae271e1f0013368af31c7c6f900ce
Shawn Tung 董劭杰 <67937453+pourquoiaimer@users.noreply.github.com>
2021-01-22 11:39:39 +0800

    改一些註解

--- 1.九九乘法表/all.js
+++ 1.九九乘法表/all.js
@@ -1,18 +1,18 @@
-const content = document.querySelector('.content');
+const content = document.querySelector('.content'); //確定指向要填入資料的標籤
 
-function count(num){
+function count(num){ //用來生成每個數字的乘法表內容的小迴圈
     let countStr = '';
     let innerStr = '';
-    for (let i = 1; i < 10; i++) {
+    for (let i = 1; i < 10; i++) {  //這邊會把每一個帶入的num都跑一次乘1到乘9的過程，並生成相應字串
         countStr += `<div class="number-count">${num} x ${i} = ${num * i }</div>`
     }
-    innerStr = `<div class="number-block block"><div class="number-title">${num}</div>${countStr}</div>`;
+    innerStr = `<div class="number-block block"><div class="number-title">${num}</div>${countStr}</div>`; //將上面的乘法字串加入大數字形成完整的區域
     return innerStr
 }
 
-function createdAll() {
+function createdAll() {//用迴圈去跑上面count函式的方式，依序生成2到9的乘法表，並組成字串填入相應位置
     let str = '';
-    for (let i = 2 ; i<10; i++){
+    for (let i = 2 ; i<10; i++){ 
         str += `${count(i)}`
     }
     content.innerHTML =`            <div class="title-block block">
@@ -27,8 +27,6 @@ function createdAll() {
 </div>${str}` ;
 };
 
-createdAll();
-
-
+createdAll(); //創造
 
 
